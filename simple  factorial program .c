9984570de9c0bd8d4afce88a6a813e05510a3b20
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-08-23 17:59:02 +0530

    Create simple  factorial program .c

---
+++ simple  factorial program .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n,i,fact=1;
+    printf("enter a number  whose you want to find the factorial  ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+        
+    }
+    printf("factorial is %d",fact);
+    
+    
+}
