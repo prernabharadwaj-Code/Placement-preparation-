e46efbfa373cedebf535354842c70098cd6fa6c0
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-08-24 09:43:09 +0530

    Create pattern 2.c

---
+++ pattern 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    for (i=1;i<=4;i++)
+    {
+        for(j=0;j<=4;j++)
+        {
+          printf("%d",i)  ;
+          
+        }
+        printf("\n");
+        }
+    
+}
